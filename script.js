ea56bb1f40c4c5d010dba47b367d91eda4abb94d
panhwar11090 <11090@kiet.edu.pk>
2023-08-26 01:35:52 +0500

    js completed

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let selectedWord= words[Math.floor(Math.random()*words.length)];
 
 
 // Tracking arrays for correct and incorrect guess
-const correctLetterArray = ['a','b','c','d','e','f'];
+const correctLetterArray = [];
 const incorrectLetterArray = [];
 
 // function to display the selected word in the dom
@@ -53,7 +53,85 @@ function displayWord(){
     }
 };
 
+// Function to show notificatio
+function showNotification(){
+    //Add class show to the notification container
+    notification.classList.add('show');
+    // After 2 seconds hiding it
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
+}
 
+// Function to update incorrect letters
+function updateIncorrectLetter(){
+    // Display the incorrect letters
+    incorrectLetters.innerHTML=`
+        ${incorrectLetterArray.length > 0 ? '<p>Incorrect Letters</p>' : ''}
+        ${incorrectLetterArray.map(letter => `<span>${letter}</span>`)}
+    `;
+    //Display the hangman part
+    figureParts.forEach((part , index) =>{
+        //How many incorrect letters has the user gussed
+        const errors = incorrectLetterArray.length;
+        if(index < errors){
+            part.style.display = 'block';
+        } else{
+            part.style.display = 'none';
+        }
+    });
+    // check if user lostr
+    if (incorrectLetterArray.length === figureParts.length ) {
+        finalMessage.innerText = 'You Lost'
+        popup.style.display = 'flex';
+    }
+}
+
+// Event Handler
+//1 Listen for keyboar key press
+window.addEventListener('keydown', e =>{
+    //check if key pressed is a letter a = 65 to z =90
+    if(e.keyCode >= 65 && e.keyCode <= 90){
+        const letter = e.key;
+        // Check if letter is in the selected word
+        if(selectedWord.includes(letter)){
+            // check if leter is alredy in correctletter array
+            if(!correctLetterArray.includes(letter)){
+                // add leter into the correct array
+                correctLetterArray.push(letter);
+                // run the dispalyword fucntion again to display new lette
+                displayWord();
+            } else{
+                showNotification();
+            }
+        }else{
+            //check if letter is alredy in incorrext Array
+            if(!incorrectLetterArray.includes(letter)){
+                // add letter inot the inncorect array
+                incorrectLetterArray.push(letter);
+                // update the incorretc letter ui
+                updateIncorrectLetter();
+            } else{
+                showNotification();
+            }
+        }
+    }
+});
+
+// 2. Listen for click on play again button
+playBtn.addEventListener('click', () => {
+    // Empty correctLettersArray & incorrectLettersArray
+    correctLetterArray.splice(0);
+    incorrectLetterArray.splice(0);
+    // Select a new random word
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+    // Clear incorrect letters display
+    updateIncorrectLetter();
+    // Hide the popup
+    popup.style.display = 'none';
+    // refresh displayed word
+    displayWord();
+})
 
 
 displayWord();
